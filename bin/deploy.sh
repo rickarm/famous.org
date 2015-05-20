2aa4a31b08a89ac632b99d1c3e2b3aa0566f6ddf
Marianna Bezler <marianna@famo.us>
2015-05-20 13:01:16 -0700

    chore: deploy by branch [#95085994]

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,2 +1,22 @@
-npm run build
-aws s3 --profile=famous --acl=public-read cp --recursive ./public s3://staging.famous.org/
+#!/usr/bin/env bash
+
+#
+# Script is expecting branch as an argument: origin/master or origin/develop.
+# Jenkins is going to look after the project and run the script with the appropriate parameter.
+########################################
+npm install;
+npm run build;
+
+if [[ $# > 0 ]]; then
+    if [[ "$1" == "origin/master" ]]; then
+        echo "Deploying to Production";
+        aws s3 --profile=famous --acl=public-read cp --recursive ./public s3://famous.org/
+    else
+        if [[ "$1" == "origin/develop" ]]; then
+            echo "Deploying to Staging";
+            aws s3 --profile=famous --acl=public-read cp --recursive ./public s3://staging.famous.org/
+        else
+            echo "Unknown branch. Deploy is terminated.";
+        fi
+    fi
+fi
