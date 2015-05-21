69105ba36aa4d6a826ae2f8b42b3700e37475661
Myles Borins <myles@famo.us>
2015-05-21 01:19:22 -0700

    finish cleaning up deploy scripts alis deploy-ci

--- bin/deploy-ci.sh
+++ bin/deploy-ci.sh
@@ -4,19 +4,4 @@
 # Script is expecting branch as an argument: origin/master or origin/develop.
 # Jenkins is going to look after the project and run the script with the appropriate parameter.
 ########################################
-npm install;
-npm run build;
-
-if [[ $# > 0 ]]; then
-    if [[ "$1" == "origin/master" ]]; then
-        echo "Deploying to Production";
-        aws s3 --region=us-west-1 --acl=public-read cp --recursive ./public s3://famous.org/
-    else
-        if [[ "$1" == "origin/develop" ]]; then
-            echo "Deploying to Staging";
-            aws s3 --region=us-west-1 --acl=public-read cp --recursive ./public s3://staging.famous.org/
-        else
-            echo "Unknown branch. Deploy is terminated.";
-        fi
-    fi
-fi
+./bin/deploy
