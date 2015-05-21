69bccdf63ed2965dfc5071c31e6d0fb20dabff68
Myles Borins <myles@famo.us>
2015-05-21 01:15:49 -0700

    squash

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 
 npm run build
-aws s3 --profile=famous --acl=public-read cp --recursive ./public s3://staging.famous.org/
+
+if branch=$(git symbolic-ref --short -q HEAD)
+then
+  echo on branch $branch
+  if [[ "$branch" == "master" ]]; then
+      echo "Deploying to Production";
+      aws s3 --region=us-west-1 --acl=public-read cp --recursive ./public s3://famous.org/
+  else
+      if [[ "$1" == "develop" ]]; then
+          echo "Deploying to Staging";
+          aws s3 --region=us-west-1 --acl=public-read cp --recursive ./public s3://staging.famous.org/
+      else
+          echo "Unknown branch. Deploy is terminated.";
+      fi
+  fi
+else
+  echo not on any branch
+fi
