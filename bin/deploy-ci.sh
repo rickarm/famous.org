b521ff06bf1386a295cc94230b60a8af777be4fd
Myles Borins <myles@famo.us>
2015-05-22 14:52:19 -0700

    Remove redundant script

--- bin/deploy-ci.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#
-# Script is expecting branch as an argument: origin/master or origin/develop.
-# Jenkins is going to look after the project and run the script with the appropriate parameter.
-########################################
-./bin/deploy
